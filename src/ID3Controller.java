e41a6977aeafa15c93ffb3f4f400b5ae1353c82c
conchulio <waltersheridan@gmail.com>
2012-10-23 14:03:44 +0200

    First commit

---
+++ src/ID3Controller.java
@@ -0,0 +1,12 @@
+public class ID3Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO: fileChooser in it's own window, 
+		// then a window to edit something shows up.
+		ID3View mainWindow = new ID3View();
+		mainWindow.setVisible(true);
+	}
+}
---
+++ src/ID3View.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.io.File;
+
+public class ID3View extends JFrame {
+	public ID3View() {
+		GridLayout mainGrid = new GridLayout(1,2);
+		setLayout(mainGrid);
+		
+		File homeDir = FileSystemView.getFileSystemView().getHomeDirectory();
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.add(new JLabel("Spam and eggs"));
+		JPanel rightPanel = new JPanel();
+		rightPanel.add(new Button("Just for fun"));
+		
+		add(leftPanel);
+		add(rightPanel);
+		setSize(500, 500);
+		setTitle("awesome-id3");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
